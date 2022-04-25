cdb1a10b8ec2c9d7acafccd4374f41cabbcff671
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2022-04-25 17:32:15 +0800

    refactor: pull some logic into a common module

--- imageio-jxl/src/test/java18/com/github/gotson/nightmonkeys/jxl/imageio/plugins/JpegXlImageReaderTest.java
+++ imageio-jxl/src/test/java18/com/github/gotson/nightmonkeys/jxl/imageio/plugins/JpegXlImageReaderTest.java
@@ -1,19 +1,17 @@
 package com.github.gotson.nightmonkeys.jxl.imageio.plugins;
 
-import com.github.gotson.nightmonkeys.jxl.TestUtils;
+import com.github.gotson.nightmonkeys.common.imageio.ImageReaderBaseTest;
 import org.junit.jupiter.api.Test;
 
 import javax.imageio.ImageIO;
 import javax.imageio.ImageReader;
-import javax.imageio.stream.ImageInputStream;
 import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-class JpegXlImageReaderTest {
+class JpegXlImageReaderTest extends ImageReaderBaseTest<JxlImageReader> {
 
     @Test
     public void testReaderIsRegistered() {
@@ -41,29 +39,20 @@ class JpegXlImageReaderTest {
             .hasAtLeastOneElementOfType(JxlImageReader.class);
     }
 
-    private JxlImageReader getReader(String fixtureFile) throws IOException {
-        File inFile = TestUtils.getRessourceFile(fixtureFile);
-        ImageInputStream is = ImageIO.createImageInputStream(inFile);
-        ImageReader reader = ImageIO.getImageReaders(is).next();
-        assertThat(reader).isInstanceOf(JxlImageReader.class);
-        reader.setInput(is);
-        return (JxlImageReader) reader;
-    }
-
     @Test
     public void testRead() throws IOException {
-        BufferedImage img = ((ImageReader) getReader("hills.jxl")).read(0, null);
+        BufferedImage img = getReader("/hills.jxl").read(0, null);
         assertThat(img.getWidth()).isEqualTo(610);
         assertThat(img.getHeight()).isEqualTo(407);
     }
 
     @Test
     public void testCanReuseReader() throws Exception {
-        ImageReader reader = getReader("hills.jxl");
+        ImageReader reader = getReader("/hills.jxl");
         BufferedImage rgbImg = reader.read(0, null);
 
         reader.setInput(
-            ImageIO.createImageInputStream(TestUtils.getRessourceFile("island.jxl")));
+            ImageIO.createImageInputStream(getClassLoaderResourceAsFile("/island.jxl")));
         BufferedImage bwImg = reader.read(0, null);
 
         assertThat(rgbImg.getRGB(256, 256)).isNotEqualTo(bwImg.getRGB(256, 256));
