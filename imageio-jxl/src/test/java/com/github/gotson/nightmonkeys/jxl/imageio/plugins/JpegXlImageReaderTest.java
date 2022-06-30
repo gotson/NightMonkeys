ba6b6dd09a86776c0740553a1cfd00873499a247
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2022-06-30 14:31:43 +0800

    refactor: centralize no op tests in a base abstract reader

--- imageio-jxl/src/test/java/com/github/gotson/nightmonkeys/jxl/imageio/plugins/JpegXlImageReaderTest.java
+++ imageio-jxl/src/test/java/com/github/gotson/nightmonkeys/jxl/imageio/plugins/JpegXlImageReaderTest.java
@@ -1,41 +1,24 @@
 package com.github.gotson.nightmonkeys.jxl.imageio.plugins;
 
-import org.apache.commons.collections4.IteratorUtils;
-import org.junit.jupiter.api.Test;
+import com.github.gotson.nightmonkeys.common.imageio.NoOpImageReaderAbstractTest;
 
-import javax.imageio.ImageIO;
-import javax.imageio.ImageReader;
-import java.io.IOException;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.Enumeration;
-import java.util.Spliterator;
-import java.util.Spliterators;
-import java.util.stream.StreamSupport;
+import java.util.Collections;
+import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThat;
+class JpegXlImageReaderTest extends NoOpImageReaderAbstractTest<JxlImageReader> {
 
-class JpegXlImageReaderTest {
+    @Override
+    protected List<String> getFormatNames() {
+        return Collections.singletonList("jxl");
+    }
 
-    @Test
-    public void testReaderIsNotRegistered() throws IOException {
-        System.out.println("java.version: " + System.getProperty("java.version"));
-        System.out.println("java.library.path: " + System.getProperty("java.library.path"));
-        Enumeration<URL> spiFiles = ClassLoader.getSystemResources("META-INF/services/javax.imageio.spi.ImageReaderSpi");
-        Spliterator<URL> spliterator = Spliterators.spliteratorUnknownSize(IteratorUtils.asIterator(spiFiles), Spliterator.ORDERED);
-        StreamSupport.stream(spliterator, false).forEachOrdered(x -> System.out.println("SPI File: " + x));
+    @Override
+    protected List<String> getSuffixes() {
+        return Collections.singletonList("jxl");
+    }
 
-        ArrayList<ImageReader> readers = new ArrayList<>();
-        ImageIO.getImageReadersBySuffix("jxl").forEachRemaining(readers::add);
-
-        assertThat(readers).isEmpty();
-
-        ImageIO.getImageReadersByMIMEType("image/jxl").forEachRemaining(readers::add);
-
-        assertThat(readers).isEmpty();
-
-        ImageIO.getImageReadersByFormatName("jxl").forEachRemaining(readers::add);
-
-        assertThat(readers).isEmpty();
+    @Override
+    protected List<String> getMIMETypes() {
+        return Collections.singletonList("image/jxl");
     }
 }
